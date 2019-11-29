385e54c00b359a6eb63840c494d7e4e1dc0f8fce
jesusafu <123jesusafu@gmail.com>
2019-11-29 13:37:35 -0700

    Se agrego una escena llamada combate, dos prefabs para el enemigo y el jugador, y 2 scripts uno para unit, y otro para el sistema de batalla, por ahora solo aparecen en posicion los modelos

---
+++ Assets/Scripts/Combat/BattleSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour {
+
+    public BattleState state;
+
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerTile;
+    public Transform enemyTile;
+
+    // Start is called before the first frame update
+    void Start () {
+        state = BattleState.START;
+        SetupBattle ();
+    }
+
+    void SetupBattle () {
+        GameObject pgo = Instantiate(playerPrefab, playerTile);
+        GameObject ego = Instantiate(enemyPrefab, enemyTile);
+    }
+
+}
---
+++ Assets/Scripts/Combat/Unit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+    public string unitName;
+    public int hp;
+    public int initialHp;
+    public int dmg;
+    public int dfs;
+    public int pos;
+}
